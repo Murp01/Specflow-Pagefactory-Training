4d7ab809c69f4ea4235da29ed66a2d2da2592d39
Murp01 <pmurphy35@hotmail.com>
2019-06-15 20:11:24 +0100

    Add project files.

---
+++ Specflow-Pagefactory-Training/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Specflow-Pagefactory-Training")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Specflow-Pagefactory-Training")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d5216d7f-1a02-44ba-abd8-3b27abed2bd2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Specflow-Pagefactory-Training/TestCases/LoginTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Specflow_Pagefactory_Training.TestCases
+{
+    class LoginTest
+    {
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new FirefoxDriver();
+            driver.Url = "http://www.store.demoqa.com";
+
+            // Find the element that's ID attribute is 'account'(My Account) 
+            //driver.FindElement(By.XPath(".//*[@id='account']/a")).Click();
+
+            // Find the element that's ID attribute is 'log' (Username)
+            // Enter Username on the element found by above desc.
+            //driver.FindElement(By.Id("log")).SendKeys("testuser_1");
+
+            // Find the element that's ID attribute is 'pwd' (Password)
+            // Enter Password on the element found by the above desc.
+            //driver.FindElement(By.Id("pwd")).SendKeys("Test@123");
+
+            // Now submit the form.
+            //driver.FindElement(By.Id("login")).Click();
+
+            // Find the element that's ID attribute is 'account_logout' (Log Out)
+            //driver.FindElement(By.XPath(".//*[@id='account_logout']/a")).Click();
+
+            // Close the driver
+            driver.Quit();
+
+        }
+    }
+}
